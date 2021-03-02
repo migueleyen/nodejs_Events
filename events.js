87cc0e897c9713a8f153ff5acec4458eb035ea94
migueleyen <migueleyen@gmail.com>
2021-03-02 15:00:09 -0500

    second event emit data

--- events.js
+++ events.js
@@ -4,13 +4,13 @@ const emitter = new eventEmitter();
 
 //Event Listener was Called - Before emit Event - Console Response
 
-emitter.on("TrigerEvent", function () {
-  console.log("Listener Called ");
+emitter.on("TrigerEvent", function (data) {
+  console.log("Listener Called ", data);
 });
 
 // Event Trigger - Raise and Event -Send Signals
 
-emitter.emit("TrigerEvent");
+emitter.emit("TrigerEvent", { id: 1, url: "htttp://.." });
 
 /* //Event Listener was Called - After emit Event - Console Do Nothing 
 emitter.on("TrigerEvent", function () {
