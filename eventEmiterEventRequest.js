67e6203207e667c4ccdb17e5e39fe37fabd01592
migueleyen <migueleyen@gmail.com>
2021-03-02 18:49:03 -0500

    eventEmiter.on Logged

--- eventEmiterEventRequest.js
+++ eventEmiterEventRequest.js
@@ -4,15 +4,20 @@ const events = require("events");
 const eventEmitter = new events.EventEmitter();
 
 const server = http.createServer(function (req, res) {
-  // Event Listener
-  eventEmitter.on("user request", function () {
-    console.log("A request has been done on the server !");
-  });
+  // Event Trigger after Event Listener Logged
+
+  eventEmitter.emit("user request");
+
   res.end("Server Works !");
 });
-// Event Trigger
-eventEmitter.emit("user request");
 
+// Event Listener Logged:event.Emitter.on
+
+eventEmitter.on("user request", function () {
+  console.log("A request has been done on the server !");
+});
+
+//Run Start Listen Server
 server.listen(3000, "localhost", function () {
   console.log("Server Start listening on localhost:3000");
 });
